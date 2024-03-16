14439c095bfac0ab9fc11409c37d40b983120192
Albert Graef <aggraef@gmail.com>
2024-03-16 16:25:08 +0100

    Update the Launch Control XL map.
    This uses the new and tidier output format. Also added the transport
    rewind and pos/anacrusis functions which are useful as stutter and scrub
    controls in live performance. Note that the raptor toggle was moved to
    the send select "down" button so that we can assign a fader to the pos
    control.

--- data/launchcontrol_map.lua
+++ data/launchcontrol_map.lua
@@ -3,127 +3,60 @@
 -- the LaunchControl XL. Enjoy. :) Albert Gräf <agggraef@gmail.com>, 2024
 
 -- This maps the encoders in each column to the controls of the corresponding
--- row in the panel panel (1st column A = minvel, B = maxvel, Pan = velmod;
--- 2nd column = pmin, pmax, pmod, etc.). This works quite well, since the pan
--- pots are mapped to the mod controls in the third column of the panel, which
--- are mostly bipolar. The Send A and B controls are mostly mapped to ranges,
--- so that A controls the lower, B the upper value. Some rows have only a
--- single value (besides the mod value), in which case I mapped both A and B
--- to that value.
+-- row in the panel (1st column A = minvel, B = maxvel, Pan = velmod; 2nd
+-- column = pmin, pmax, pmod, etc.). This works quite well, since the pan pots
+-- are mapped to the mod controls in the third column of the panel, which are
+-- mostly bipolar. The Send A and B controls are mostly mapped to ranges, so
+-- that A controls the lower, B the upper value. Some rows have only a single
+-- value (besides the mod value), in which case I mapped both A and B to that
+-- value.
 
--- The faders are mapped as follows: 1 = hi, 2 = lo, 3 = raptor toggle, 4 =
--- mode (random, up, down, etc.), 5 = loop size, 6 = meter numerator, 7 =
--- meter denominator, 8 = division. Note that the toggles will be engaged for
--- any non-zero value, so they can easily be turned on and off with just a
--- tiny fader movement.
+-- The faders are mapped as follows: 1 = hi, 2 = lo, 3 = mode (random, up,
+-- down, etc.), 4 = pos/anacrusis (the "scrubbing" control), 5 = loop size,
+-- 6 = meter numerator, 7 = meter denominator, 8 = division.
 
--- Finally, the track select buttons are mapped to the transport and loop
--- controls (left = loop, right = play).
+-- The four remaining CC buttons (send and track select) are mapped as follows:
+-- up = rewind, down = raptor toggle, left = loop, right = play.
 
--- You might want to remap some or all of these, if you use other controls
--- more often.
+-- I think that this covers the most important functions that you might want
+-- to use during a live performance. Of course, YMMV, so feel free to change
+-- these around to better suit your style.
 
 {
-  [13] = {
-    [9] = "minvel"
-  },
-  [14] = {
-    [9] = "pmin"
-  },
-  [15] = {
-    [9] = "wmin"
-  },
-  [16] = {
-    [9] = "gate"
-  },
-  [17] = {
-    [9] = "hmin"
-  },
-  [18] = {
-    [9] = "pref"
-  },
-  [19] = {
-    [9] = "smin"
-  },
-  [20] = {
-    [9] = "uniq"
-  },
-  [29] = {
-    [9] = "maxvel"
-  },
-  [30] = {
-    [9] = "pmax"
-  },
-  [31] = {
-    [9] = "wmax"
-  },
-  [32] = {
-    [9] = "gate"
-  },
-  [33] = {
-    [9] = "hmax"
-  },
-  [34] = {
-    [9] = "pref"
-  },
-  [35] = {
-    [9] = "smax"
-  },
-  [36] = {
-    [9] = "nmax"
-  },
-  [49] = {
-    [9] = "velmod"
-  },
-  [50] = {
-    [9] = "pmod"
-  },
-  [51] = {
-    [9] = "gain"
-  },
-  [52] = {
-    [9] = "gatemod"
-  },
-  [53] = {
-    [9] = "hmod"
-  },
-  [54] = {
-    [9] = "prefmod"
-  },
-  [55] = {
-    [9] = "smod"
-  },
-  [56] = {
-    [9] = "nmod"
-  },
-  [77] = {
-    [9] = "pitchhi"
-  },
-  [78] = {
-    [9] = "pitchlo"
-  },
-  [79] = {
-    [9] = "raptor"
-  },
-  [80] = {
-    [9] = "mode"
-  },
-  [81] = {
-    [9] = "loopsize"
-  },
-  [82] = {
-    [9] = "meter-num"
-  },
-  [83] = {
-    [9] = "meter-denom"
-  },
-  [84] = {
-    [9] = "division"
-  },
-  [106] = {
-    [9] = { "loop", true }
-  },
-  [107] = {
-    [9] = { "play", true }
-  }
+  [13] = { [9] = "minvel" },
+  [14] = { [9] = "pmin" },
+  [15] = { [9] = "wmin" },
+  [16] = { [9] = "gate" },
+  [17] = { [9] = "hmin" },
+  [18] = { [9] = "pref" },
+  [19] = { [9] = "smin" },
+  [20] = { [9] = "uniq" },
+  [29] = { [9] = "maxvel" },
+  [30] = { [9] = "pmax" },
+  [31] = { [9] = "wmax" },
+  [32] = { [9] = "gate" },
+  [33] = { [9] = "hmax" },
+  [34] = { [9] = "pref" },
+  [35] = { [9] = "smax" },
+  [36] = { [9] = "nmax" },
+  [49] = { [9] = "velmod" },
+  [50] = { [9] = "pmod" },
+  [51] = { [9] = "gain" },
+  [52] = { [9] = "gatemod" },
+  [53] = { [9] = "hmod" },
+  [54] = { [9] = "prefmod" },
+  [55] = { [9] = "smod" },
+  [56] = { [9] = "nmod" },
+  [77] = { [9] = "pitchhi" },
+  [78] = { [9] = "pitchlo" },
+  [79] = { [9] = "mode" },
+  [80] = { [9] = "pos" },
+  [81] = { [9] = "loopsize" },
+  [82] = { [9] = "meter-num" },
+  [83] = { [9] = "meter-denom" },
+  [84] = { [9] = "division" },
+  [104] = { [9] = { "rewind", true } },
+  [105] = { [9] = { "raptor", true } },
+  [106] = { [9] = { "loop", true } },
+  [107] = { [9] = { "play", true } },
 }
